c6665dbe0b053fb7c402dbc774360199339be33a
crypt <arief.darma@proton.me>
2023-08-22 21:25:56 +0700

    install orm prisma (postgresql)

--- components/modal/store-modal.tsx
+++ components/modal/store-modal.tsx
@@ -4,8 +4,16 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useStoreModal } from '@/hooks/use-store-modal';
 import { Modal } from '../ui/modal';
 import { useForm } from 'react-hook-form';
-import { Form, FormControl, FormField, FormItem, FormLabel } from '../ui/form';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '../ui/form';
 import { Input } from '../ui/input';
+import { Button } from '../ui/button';
 
 const formSchema = z.object({
   name: z.string().min(1),
@@ -46,9 +54,16 @@ export const StoreModal = () => {
                     <FormControl>
                       <Input placeholder="E-commerce" {...field} />
                     </FormControl>
+                    <FormMessage />
                   </FormItem>
                 )}
               />
+              <div className="pt-6 space-x-2 flex items-center jutify-end w-full">
+                <Button variant="outline" onClick={storeModal.onClose}>
+                  Cencel
+                </Button>
+                <Button type="submit">Continue</Button>
+              </div>
             </form>
           </Form>
         </div>

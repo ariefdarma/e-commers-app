42d11d360ed73891b3c83d68d610cd460e4db75f
crypt <arief.darma@proton.me>
2023-08-22 20:56:40 +0700

    bug fix, store-modal.tsx mising typr Form

--- components/modal/store-modal.tsx
+++ components/modal/store-modal.tsx
@@ -4,7 +4,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useStoreModal } from '@/hooks/use-store-modal';
 import { Modal } from '../ui/modal';
 import { useForm } from 'react-hook-form';
-import { FormControl, FormField, FormItem, FormLabel } from '../ui/form';
+import { Form, FormControl, FormField, FormItem, FormLabel } from '../ui/form';
 import { Input } from '../ui/input';
 
 const formSchema = z.object({
@@ -35,20 +35,22 @@ export const StoreModal = () => {
     >
       <div>
         <div className="space-y-4 py-2 pb-4">
-          <form onSubmit={form.handleSubmit(onSubmit)}>
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Name</FormLabel>
-                  <FormControl>
-                    <Input placeholder="E-commerce" {...field} />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-          </form>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="E-commerce" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
         </div>
       </div>
     </Modal>

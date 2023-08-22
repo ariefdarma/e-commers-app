331d829549a5607218c0198b101ebe77f2054f98
crypt <arief.darma@proton.me>
2023-08-22 20:18:22 +0700

    shadcn bug fix still in progress

--- components/modal/store-modal.tsx
+++ components/modal/store-modal.tsx
@@ -1,18 +1,56 @@
 'use client';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useStoreModal } from '@/hooks/use-store-modal';
 import { Modal } from '../ui/modal';
+import { useForm } from 'react-hook-form';
+import { FormControl, FormField, FormItem, FormLabel } from '../ui/form';
+import { Input } from '../ui/input';
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
 
 export const StoreModal = () => {
   const storeModal = useStoreModal();
 
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+    // TODO: create Store
+  };
+
   return (
     <Modal
-      title="Creat Store"
-      description="Add a new store to manage products and category"
+      title="Create Store"
+      description="Add a new store to manage products and categories"
       isOpen={storeModal.isOpen}
       onClose={storeModal.onClose}
     >
-      Future Creat Store Form
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="E-commerce" {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </form>
+        </div>
+      </div>
     </Modal>
   );
 };
